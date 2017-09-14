d54407dd79083e009f69c5ed42b124e8607c8c47
Masato Kataoka <masato.kataoka@di-v.co.jp>
2017-09-14 18:05:08 +0900

    ユーザー情報編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def sign_in
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+#    if @user.update_attributes(params.require(:user).permit(:name, :email))
+    if @user.update_attributes(user_params)
+      # 更新に成功したときの処理
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:edit, :update]
   devise_for :users
   get 'messages/index'
 
