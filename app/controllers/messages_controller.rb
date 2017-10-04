7ff746fb257d95239316890a243416375f7de84a
Masato Kataoka <masato.kataoka@di-v.co.jp>
2017-10-04 21:21:10 +0900

    form部分をform_for利用に変更・messageモデル作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,8 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+  end
+  def create
+    @message = Message.new(params.require(:message).permit(:title, :body))
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912095352) do
+ActiveRecord::Schema.define(version: 20171004103014) do
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text "body"
+    t.string "image"
+    t.integer "group_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
