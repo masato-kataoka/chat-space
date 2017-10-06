fae5a4a9d78380dddc2b74249e0a61c37ec32971
Masato Kataoka <masato.kataoka@di-v.co.jp>
2017-10-06 20:30:09 +0900

    グループチャット用ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  get '/chat_groups/new', to: 'group#new'
+  post '/chat_groups', to: 'group#create'
+  get '/chat_groups/:id/messages', to: 'group#index'
+  post '/chat_groups/:id/messages', to: 'group#update'
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
