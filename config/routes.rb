464c18edd21fd4bb2cc4ecffc84b55001bb72fb9
Masato Kataoka <masato.kataoka@di-v.co.jp>
2017-10-11 20:12:42 +0900

    ルーティングファイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root to: "messages#index"
   resources :users, only: [:edit, :update]
-  get '/chat_groups/new', to: 'group#new'
-  post '/chat_groups', to: 'group#create'
-  get '/chat_groups/:id/edit', to: 'group#edit'
-  get '/chat_groups/:id/messages', to: 'group#index'
-  post '/chat_groups/:id/messages', to: 'group#update'
+  resources :group, only: [:new, :create, :edit, :update, :index]
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
