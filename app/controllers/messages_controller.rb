3646289ef9b70d1915093f8d8b9e52dbf5e1a89e
Masato Kataoka <masato.kataoka@di-v.co.jp>
2017-10-26 17:25:08 +0900

    指摘事項の修正
    - ユーザ名を出力を@groupから取り出し＋pluck利用に変更
    - newアクションの@usersが不要になったので削除
    - 複数箇所で使われている定義をbefore_actionに移行
    - set_message_params のpermit設定修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,9 @@
 class MessagesController < ApplicationController
   before_action :group_find
+  before_action :new_message, only: [:index, :new]
+  before_action :current_user_groups, only: [:index, :new]
 
   def index
-    new_message
-    current_user_groups
   end
   def create
     @message = Message.new(set_message_params)
@@ -14,10 +14,7 @@ class MessagesController < ApplicationController
     end
   end
   def new
-    @users = @group.users.map(&:name)
     @messages = Message.where(group_id: params[:group_id])
-    current_user_groups
-    new_message
   end
   def edit
   end
@@ -33,7 +30,7 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
   end
   def set_message_params
-    params.require(:message).permit(:body, :image, :group_id, :user_id).merge( {group_id: params[:group_id], user_id: current_user.id} )
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
   def current_user_groups
     @groups = current_user.groups
