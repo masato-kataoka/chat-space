f5fe1e51efda79f7cbecfb12ce3e97b8239518cd
Masato Kataoka <masato.kataoka@di-v.co.jp>
2017-10-26 14:43:41 +0900

    current_user.groupsをprivate関数化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @group = current_user.group
+    @groups = current_user_groups
   end
   def create
     @message = Message.new(body: set_message_params[:body], image: set_message_params[:image], group_id: params[:group_id], user_id: current_user.id)
@@ -14,7 +14,7 @@ class MessagesController < ApplicationController
     end
   end
   def new
-    @groups = current_user.groups
+    @groups = current_user_groups
     @users = @group.users.map(&:name)
     @messages = Message.where(group_id: params[:group_id])
     @message = Message.new
@@ -28,10 +28,14 @@ class MessagesController < ApplicationController
   end
 
   private
+
   def group_find
     @group = Group.find(params[:group_id])
   end
   def set_message_params
     params.require(:message).permit(:body, :image, :group_id, :user_id)
+  end
+  def current_user_groups
+    current_user.groups
   end
 end
