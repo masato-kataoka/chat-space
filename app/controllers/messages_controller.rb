543f395d4ffe601849f3bd4e14e2d0e60727ce8c
Masato Kataoka <masato.kataoka@di-v.co.jp>
2017-10-26 14:18:44 +0900

    修正事項の修正
    - before_actionの利用
    - @messageの保存をcreate -> newとsaveの利用に変更
    - ストロングパラメータへの修正
    - newのviewで@group.id -> @group に変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,19 @@
 class MessagesController < ApplicationController
+  before_action :group_find
+
   def index
     @message = Message.new
     @group = current_user.group
   end
   def create
-    Message.create(body: params.require(:message)[:body], image: params.require(:message)[:image], group_id: params[:group_id], user_id: current_user.id)
-    redirect_to new_group_messages_path
+    @message = Message.new(body: set_message_params[:body], image: set_message_params[:image], group_id: params[:group_id], user_id: current_user.id)
+    if @message.save
+      redirect_to new_group_messages_path
+    else
+      render :new
+    end
   end
   def new
-    group_find
     @groups = current_user.groups
     @users = @group.users.map(&:name)
     @messages = Message.where(group_id: params[:group_id])
@@ -20,11 +25,13 @@ class MessagesController < ApplicationController
     @message = Group.find(params[:id])
   end
   def show
-    group_find
   end
 
   private
   def group_find
     @group = Group.find(params[:group_id])
+  end
+  def set_message_params
+    params.require(:message).permit(:body, :image, :group_id, :user_id)
   end
 end
