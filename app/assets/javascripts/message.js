e46dc9a7fdcc28f013dac3b8d5ae1b9e4a987c68
Masato Kataoka <masato.kataoka@di-v.co.jp>
2018-01-21 23:56:18 +0900

    message#createを動くようにする・respond_toによる分岐実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,30 @@
 $(function() {
+
+  function buildHTML(data) {
+//    return html;
+  }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    var textField = $('#message_body');
-    message = textField.val();
-    console.log(message);
+    var formData = new FormData($(this).get(0));
+//    var group_id = $('.group_id').attr('value');
+//    var url = '/groups/' + group_id + '/messages.json'
+    $.ajax({
+      type: 'POST',
+      url: '../messages#create.json',
+      data: formData,
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+//      console.log(data);
+      var html = buildHTML(data);
+//      $('#chat-message__body').append(html);
+      $('#new_message').append(formData);
+      $('#new_message').val('');
+    })
+    .fail(function() {
+      alert('エラー');
+    });
   });
 });
