7ff04c76791d8957969c8c1870a89da5e4fbaf9c
Masato Kataoka <masato.kataoka@di-v.co.jp>
2018-04-20 21:00:24 +0900

    turbolinks無効化

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,4 @@
 //
 //= require rails-ujs
 //= require jquery
-//= require jquery.turbolinks
-//= require turbolinks
 //= require_tree .
--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -22,8 +22,10 @@ $(function() {
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    var formData = new FormData(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({
+      url: url,
       type: 'POST',
       data: formData,
       dataType: 'json',
