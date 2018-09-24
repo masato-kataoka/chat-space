6801910a9e4a4cc324b3822c182d0208da52ad27
Masato Kataoka <masato.kataoka@di-v.co.jp>
2018-09-24 17:19:12 +0900

    時刻表示の処理方法を修正

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -7,7 +7,7 @@ $(function() {
                       ${message.user_name}
                     </div>
                     <div class='upper-message__date'>
-                      ${message.created_at}
+                      ${message.datetime}
                     </div>
                   </div>
                   <div class='lower-message'>
