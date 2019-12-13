d04516205f81dfab128b2b69cda47be19411497d
Masato Kataoka <>
2019-12-13 20:30:33 +0900

    logrageの設定をinitializersに移動

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # For JSON logs
-  config.lograge.enabled = true
-  config.lograge.logger = ActiveSupport::Logger.new
 end
